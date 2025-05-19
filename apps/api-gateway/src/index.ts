50d2e0ede78d18a133ddd7f544e65ead11149bbe
Luiz Felipe Moureau <xfazxtudo@gmail.com>
2025-05-19 04:28:48 -0300

    🚪 gateway: adding missing services and Dockerfile

--- apps/api-gateway/src/index.ts
+++ apps/api-gateway/src/index.ts
@@ -20,6 +20,8 @@ async function startGateway() {
     serviceList: [
       { name: "users", url: "http://localhost:4001/graphql" },
       { name: "posts", url: "http://localhost:4002/graphql" },
+      { name: "comments", url: "http://localhost:4003/graphql" },
+      { name: "categories", url: "http://localhost:4004/graphql" },
     ],
   });
 
